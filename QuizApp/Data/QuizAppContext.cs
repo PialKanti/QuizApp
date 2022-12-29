151679fc5677dbd58c7e3c2ce920eeec88d627d5
Pial Kanti Samadder <pialkanti2012@gmail.com>
2022-12-29 12:01:52 +0600

    Created Migration CreateInitialIdentityTables

--- QuizApp/Data/QuizAppContext.cs
+++ QuizApp/Data/QuizAppContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuizApp.Entities;
 
 namespace QuizApp.Data
 {
-    public class QuizAppContext : IdentityDbContext
+    public class QuizAppContext : IdentityDbContext<User>
     {
+        public QuizAppContext(DbContextOptions<QuizAppContext> options) : base(options) { }
     }
 }
