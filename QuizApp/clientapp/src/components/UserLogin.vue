d500f0b64c4dc57c675f6a6c3103035ce7d2a0a7
Pial Kanti Samadder <pialkanti2012@gmail.com>
2022-12-29 15:42:10 +0600

    Added user login form to client app

---
+++ QuizApp/clientapp/src/components/UserLogin.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="vh-100 bg-image">
+        <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+            <div class="container h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                        <div class="card" style="border-radius: 15px;">
+                            <div class="card-body p-5">
+                                <h2 class="text-uppercase text-center mb-5">Log in</h2>
+                                <form ref="userRegistrationForm" @submit.prevent="submitForm">
+                                    <div class="form-outline mb-4">
+                                        <input type="email" id="email" class="form-control form-control-lg"
+                                            v-model="email" />
+                                        <label class="form-label" for="email">Email</label>
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="password" id="password" class="form-control form-control-lg"
+                                            v-model="password" />
+                                        <label class="form-label" for="password">Password</label>
+                                    </div>
+
+                                    <div class="d-flex justify-content-center">
+                                        <button type="submit" class="btn btn-primary btn-lg">Log in</button>
+                                    </div>
+
+                                    <p class="text-center text-muted mt-5 mb-0">Need an account? <a href="#!"
+                                            class="fw-bold text-body"><u>Sign up here</u></a></p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "UserLogin",
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    }
+}
+</script>
--- QuizApp/clientapp/src/main.js
+++ QuizApp/clientapp/src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import UserRegistration from "./components/UserRegistration.vue"
+import UserLogin from "./components/UserLogin.vue"
 
 const routes = [
     { path: '/signup', component: UserRegistration },
+    { path: '/login', component: UserLogin }
 ]
 
 const router = createRouter({
