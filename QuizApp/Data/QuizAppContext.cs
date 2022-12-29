1a4cd35c0b9021be5c71774115a77e0e8f004e43
Pial Kanti Samadder <pialkanti2012@gmail.com>
2022-12-29 15:59:17 +0600

    Added migration for Default roles and admin user

--- QuizApp/Data/QuizAppContext.cs
+++ QuizApp/Data/QuizAppContext.cs
@@ -8,5 +8,59 @@ namespace QuizApp.Data
     public class QuizAppContext : IdentityDbContext<ApplicationUser>
     {
         public QuizAppContext(DbContextOptions<QuizAppContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedDefaultAdminUser(builder);
+            SeedRoles(builder);
+            SeedDefaultAdminUserRole(builder);
+        }
+
+        private void SeedDefaultAdminUser(ModelBuilder builder)
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                Id = "b74ddd14-6340-4840-95c2-db12554843e5",
+                FirstName = "Default",
+                LastName = "Admin",
+                UserName = "admin",
+                Email = "admin@test.com",
+                LockoutEnabled = false,
+            };
+
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHasher.HashPassword(user, "admin123");
+
+            builder.Entity<ApplicationUser>().HasData(user);
+        }
+
+        private void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+                 new IdentityRole
+                 { 
+                     Id = "fab4fac1-c546-41de-aebc-a14da6895711", 
+                     Name = "Admin", 
+                     ConcurrencyStamp = "1", 
+                     NormalizedName = "Admin" 
+                 },
+                 new IdentityRole
+                 { 
+                     Id = "c7b013f0-5201-4317-abd8-c211f91b7330", 
+                     Name = "Participant", 
+                     ConcurrencyStamp = "2", 
+                     NormalizedName = "Participant"
+                 });
+        }
+
+        private void SeedDefaultAdminUserRole(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string> {
+                    UserId = "b74ddd14-6340-4840-95c2-db12554843e5",
+                    RoleId = "fab4fac1-c546-41de-aebc-a14da6895711"
+                });
+        }
     }
 }
---
+++ QuizApp/Migrations/20221229095557_SeedDefaultUserAndRoles.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace QuizApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDefaultUserAndRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "c7b013f0-5201-4317-abd8-c211f91b7330", "2", "Participant", "Participant" },
+                    { "fab4fac1-c546-41de-aebc-a14da6895711", "1", "Admin", "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "b74ddd14-6340-4840-95c2-db12554843e5", 0, "1714d9a2-29c7-4b79-a4f4-6539d092dc2b", "admin@test.com", false, "Default", "Admin", false, null, null, null, "AQAAAAEAACcQAAAAEFTgjUWOgQ3SW/AjFsRBbFCxBdYu3EuXNl6KQR+t2TQcA0bAu7gFWFjT6ayTrivYLA==", null, false, "0f2e036b-28d8-42f2-9780-e1018d9733e4", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "fab4fac1-c546-41de-aebc-a14da6895711", "b74ddd14-6340-4840-95c2-db12554843e5" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c7b013f0-5201-4317-abd8-c211f91b7330");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "fab4fac1-c546-41de-aebc-a14da6895711", "b74ddd14-6340-4840-95c2-db12554843e5" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fab4fac1-c546-41de-aebc-a14da6895711");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b74ddd14-6340-4840-95c2-db12554843e5");
+        }
+    }
+}
